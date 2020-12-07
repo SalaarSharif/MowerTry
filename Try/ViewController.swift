9370ca66564b5562d1a5662e857bb4cb4d504fc4
Salaar <lazzyangle@gmail.com>
2020-12-07 19:35:43 +0300

    Initial Commit

---
+++ Try/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Try
+//
+//  Created by Hazem Tariq on 12/7/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
